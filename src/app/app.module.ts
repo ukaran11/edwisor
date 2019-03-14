b7baef9c8edf76b9da4f86052c1128ee7059a860
Utsav <ukaran11@gmail.com>
2019-03-14 19:51:51 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BlogViewComponent } from './blog-view/blog-view.component';
+import { BlogCreateComponent } from './blog-create/blog-create.component';
+import { BlogEditComponent } from './blog-edit/blog-edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BlogViewComponent,
+    BlogCreateComponent,
+    BlogEditComponent
   ],
   imports: [
     BrowserModule
